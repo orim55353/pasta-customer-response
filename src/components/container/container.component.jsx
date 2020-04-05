0c277b2184d3b7cc43636f7962c64138599c7a84
Ori Mizrachi <orim553@gmail.com>
2020-04-05 16:52:13 +0300

    More Styling

--- src/components/container/container.component.jsx
+++ src/components/container/container.component.jsx
@@ -6,19 +6,18 @@ import "./container.styles.scss";
 
 class Container extends React.Component {
   answers = {};
+  questions = [
+    "האם ההזמנה בטלפון היתה נעימה וברורה",
+    "האם השליח היה אדיב",
+    "האם האוכל הגיע לשביעות רצונך",
+  ];
 
   constructor() {
     super();
     this.state = {
-      0: [],
-      1: [],
-      2: [],
-      answers: {},
-      questions: [
-        "האם ההזמנה בטלפון היתה נעימה וברורה",
-        "האם השליח היה אדיב",
-        "האם האוכל הגיע לשביעות רצונך",
-      ],
+      name: "תגובת משתמש",
+      email: "orim553@gmail.com",
+      message: "11111",
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -29,6 +28,37 @@ class Container extends React.Component {
     console.log(this.answers);
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.status);
+        if (response.status === "success") {
+          alert("Message Sent.");
+          this.resetForm();
+        } else if (response.status === "fail") {
+          alert("Message failed to send.");
+        }
+      });
+  };
+
+  componentDidMount() {
+    const script = document.createElement("script");
+
+    script.src = "https://smtpjs.com/v3/smtp.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div className="main">
@@ -36,14 +66,16 @@ class Container extends React.Component {
           הי זו עדי מפסטה לוקו רצינו לוודא שהכל היה נעים וטעים . נודה על מילוי
           משוב קצר
         </span>
-        {this.state.questions.map((question, index) => (
+        {this.questions.map((question, index) => (
           <QuestionField
             onClick={this.handleClick}
             question={question}
             index={index}
           ></QuestionField>
         ))}
-        <button>שלח</button>
+        <button onClick={this.handleSubmit} className="send">
+          שלח
+        </button>
       </div>
     );
   }
