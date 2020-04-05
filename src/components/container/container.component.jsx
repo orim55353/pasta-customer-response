d397e4b0866fdf54f46f87217e5c1a8c413f1fbb
Ori Mizrachi <orim553@gmail.com>
2020-04-05 18:40:32 +0300

    Added Email Functionallity

--- src/components/container/container.component.jsx
+++ src/components/container/container.component.jsx
@@ -16,13 +16,13 @@ class Container extends React.Component {
 
   constructor() {
     super();
+
     this.state = {
-      name: "תגובת משתמש",
-      email: "orim553@gmail.com",
-      message: "11111",
+      text: "",
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleClick(index, parentIndex) {
@@ -30,10 +30,43 @@ class Container extends React.Component {
     console.log(this.answers);
   }
 
+  handleSubmit() {
+    const templateId = "pastatemp";
+    var text = "";
+    var answers = this.answers;
+    this.questions.forEach(function (question, index) {
+      text += question + ": ";
+      text += (answers[index] ? answers[index] : "לא נענתה ") + ", \n";
+    });
+    text += "תשובה בטקסט חופשי: " + this.state.text;
+    console.log(text);
+    this.sendFeedback(templateId, {
+      send: text,
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
+  sendFeedback(templateId, variables) {
+    window.emailjs
+      .send("gmail", templateId, variables)
+      .then((res) => {
+        console.log("Email successfully sent!");
+      })
+      .catch((err) =>
+        console.error(
+          "Oh well, you failed. Here some thoughts on the error that occured:",
+          err
+        )
+      );
+  }
+
   render() {
     return (
       <div className="main">
-        <img src={Logo} />
+        <img src={Logo} alt="logo" />
         <span>
           הי זו עדי מפסטה לוקו רצינו לוודא שהכל היה נעים וטעים . נודה על מילוי
           משוב קצר
@@ -46,7 +79,10 @@ class Container extends React.Component {
           ></QuestionField>
         ))}
         <h3>מסר אישי</h3>
-        <textarea placeholder="כתוב לנו משהו אישי"></textarea>
+        <textarea
+          onChange={this.handleChange}
+          placeholder="כתוב לנו משהו אישי"
+        ></textarea>
         <button onClick={this.handleSubmit} className="send">
           שלח
         </button>
